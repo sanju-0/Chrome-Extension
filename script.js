e26932ce6e45e2d55ab7d20367c92c8b33ce628c
Sanju <anutanwer12@gmail.com>
2021-09-29 17:54:48 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+   
+fetch('https://icanhazdadjoke.com/slack')
+.then(data => data.json())
+.then(jokeData => {
+    const jokeText = jokeData.attachments[0].text;
+    const joke = document.getElementById('joke');
+
+    joke.innerHTML = jokeText;
+})
+
