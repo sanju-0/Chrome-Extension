8bd02d66e40038a57a1f58ef9d25a2a406ee358f
harris2310 <harris2310@hotmail.com>
2022-06-02 08:47:49 +0300

    Updated script.js to choose randomly from two API's up to date with 'origin/main'.
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git restore <file>..." to discard changes in working directory)
            modified:   script.js

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
-   
-fetch('https://icanhazdadjoke.com/slack')
-.then(data => data.json())
-.then(jokeData => {
-    const jokeText = jokeData.attachments[0].text;
-    const joke = document.getElementById('joke');
+const urls = ['https://icanhazdadjoke.com/slack', 
+              'https://v2.jokeapi.dev/joke/Any?blacklistFlags=religious,political,racist,sexist,explicit',
+             ];
 
-    joke.innerHTML = jokeText;
-})
+const choice =  Math.floor(Math.random() * urls.length);
+
+const urlUsed =  urls[choice];
+
+console.log(choice);
+
+if (choice == 0) {
+  fetch(urlUsed)
+  .then(data => data.json())
+  .then(jokeData => {
+      const jokeText = jokeData.attachments[0].text;
+      const joke = document.getElementById('joke');
+  
+      joke.innerHTML = jokeText;
+  })
+} else {
+  fetch(urlUsed)
+  .then(data => data.json())
+  .then(jokeData => {
+    if (jokeData.type == 'single') {
+      const jokeText = jokeData.joke;
+      const joke = document.getElementById('joke');
+
+      joke.innerHTML = jokeText;
+
+    } else {
+      const jokeText = jokeData.setup + ' ' + jokeData.delivery;
+      const joke = document.getElementById('joke');
+
+      joke.innerHTML= jokeText;
+    }
+  })
+}
 
